4372016badba7650961669b9daac30768ea223b9
kenneth <kenneth@email.com>
2018-08-15 15:27:21 +0000

    ¸Äģ°

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel-Kenneth
                 </div>
 
                 <div class="links">
